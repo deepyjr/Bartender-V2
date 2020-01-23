3a2c11e52df06c627cc2cf5d5d9b43774dff81b9
deepyjr <deepyjr@gmail.com>
2020-01-23 17:11:02 +0100

    ajout de modif sur le login

--- view/login/login.php
+++ view/login/login.php
@@ -24,21 +24,17 @@
     </svg>
   </div>
 
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
 
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
 
   <label for="inputPassword" class="sr-only">Password</label>
   <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label>
-  </div>
-
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <a href="../forms/forms.php" >S'inscrire</a>
+  
+  
+  <button class="btn btn-lg btn-primary btn-block mt-2" type="submit">Se connecter</button>
   <p class="mt-5 mb-3 text-muted">© 2017-2020</p>
 </form>
 

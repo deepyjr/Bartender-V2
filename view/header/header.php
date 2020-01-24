3654ac68a64b1d7ace9f255cbd9e7d5c5744699f
deepyjr <deepyjr@gmail.com>
2020-01-24 02:19:33 +0100

    ajout du formulaire de contact mais pb avec le header ( lien qui bug )

--- view/header/header.php
+++ view/header/header.php
@@ -18,7 +18,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="../home.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../view/home.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="../view/actual/actual.php">Commande(s) en cours</a>
@@ -30,7 +30,7 @@
         <a class="nav-link" href="#">Mes Informations</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contacter</a>
+        <a class="nav-link" href="../contact/contact.php">Contacter</a>
       </li>
     </ul>
 
